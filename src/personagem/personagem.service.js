4162ecd866d8cfad26bbb661a16bd7d488453fdd
Adonay Santos <adonay.rssantos@gmail.com>
2024-07-04 18:38:42 -0300

    UUpdateById implementado no Service e Controller

--- src/personagem/personagem.service.js
+++ src/personagem/personagem.service.js
@@ -25,7 +25,17 @@ function create(newItem) {
     return getCollection().insertOne(newItem)
 }
 
-function updateById() {
+/**
+ * 
+ * @param {string} id 
+ * @returns 
+ */
+function updateById(id, newItem) {
+    // Atualizamos no collection o novoItem pelo ID
+    return getCollection().updateOne(
+        { _id: new ObjectId(id) },
+        { $set: newItem }
+      )
 }
 
 function deleteById() {
